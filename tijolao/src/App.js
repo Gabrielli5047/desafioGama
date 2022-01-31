997adec08e393d98f34f784d228b5a7859d750dd
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-01-31 11:36:14 -0300

    celular

--- tijolao/src/App.js
+++ tijolao/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  // const [numClicados, setNumClicados] = useState("")
+  const [contador, setContador] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="celular">
+      <div className = "header-cell">
+       <input className = "input-tela"/>
+       <h2>NOKIA</h2>
+      </div>
+      <div className = "section-cell">
+      <div className = "teclas-cell">
+        <button className = "teclas">1 </button>
+        <button className = "teclas">2<p>abc</p ></button>
+        <button className = "teclas">3 <p> def</p> </button>
+      </div>
+      <div className = "teclas-cell">
+        <button className = "teclas">4 <p>ghi </p> </button>
+        <button className = "teclas">5 <p>jkl </p> </button>
+        <button className = "teclas">6 <p> mno</p> </button>
+      </div>
+      <div className = "teclas-cell">
+        <button className = "teclas">7 <p>pqrs </p> </button>
+        <button className = "teclas">8 <p>tuv </p> </button>
+        <button className = "teclas">9 <p> wxyz</p> </button>
+      </div>
+      <div className = "teclas-cell">
+        <button className = "teclas">*</button>
+        <button className = "teclas">0</button>
+        <button className = "teclas">del</button>
+      </div>
+      </div>
     </div>
   );
 }
---
+++ tijolao/src/componentes/123/index.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+
+  return (
+    
+      <div className = "teclas-cell">
+        <button className = "teclas">1 </button>
+        <button className = "teclas">2<p>abc</p ></button>
+        <button className = "teclas">3 <p> def</p> </button>
+      </div>
+      
+  );
+}
+
+export default App;
